c11842d282abaf6711b1dd24b3f21e34bbd23473
sanketsingh24 <sanketsingh24@gmail.com>
2017-12-15 17:38:05 +0530

    Added all the images

--- src/data/images.js
+++ src/data/images.js
@@ -1,16 +1,16 @@
 const imgs =  [
-  {imge:'../img/backpack.png', desc:'Backpack'},
-  {imge:'../img/brush.png', desc:'Paint Brush'},
-  {imge:'../img/burger.png', desc:'Cheeseburger'},
-  {imge:'../img/coffeemachine.png', desc:'Coffee Machine'},
-  {imge:'../img/gopro.png', desc:'GoPro'},
-  {imge:'../img/hat.png', desc:'Pirate Hat'},
-  {imge:'../img/helmet.png', desc:'Helmet'},
-  {imge:'../img/iMac.png', desc:'iMac'},
-  {imge:'../img/inkpot.png', desc:'Inkpot'},
-  {imge:'../img/notebook.png', desc:'Notebook'},
-  {imge:'../img/tv.png', desc:'TV'},
-  {imge:'../img/watch.png', desc:'Watch'}
+  {imge:'img/backpack.png', desc:'Backpack'},
+  {imge:'img/brush.png', desc:'Paint Brush'},
+  {imge:'img/burger.png', desc:'Cheeseburger'},
+  {imge:'img/coffeemachine.png', desc:'Coffee Machine'},
+  {imge:'img/gopro.png', desc:'GoPro'},
+  {imge:'img/hat.png', desc:'Pirate Hat'},
+  {imge:'img/helmet.png', desc:'Helmet'},
+  {imge:'img/iMac.png', desc:'iMac'},
+  {imge:'img/inkpot.png', desc:'Inkpot'},
+  {imge:'img/notebook.png', desc:'Notebook'},
+  {imge:'img/tv.png', desc:'TV'},
+  {imge:'img/watch.png', desc:'Watch'}
 ];
 
 export default imgs;
