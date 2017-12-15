24c45080592afab38a9a8854242eef95569cc231
sanketsingh24 <sanketsingh24@gmail.com>
2017-12-15 17:21:06 +0530

    Added functionality to nav-panel

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -16,14 +16,16 @@ class Menu extends Component {
 	shook (text,i){
 		return (
 			<div key={i}>
-			<i className="material-icons" >{text}</i>
-			<p className="nav-panel-desc">{text}</p>
+				<a href="">
+					<i className="material-icons" >{text}</i>
+					<p className="nav-panel-desc">{text}</p>
+				</a>
 			</div>
 		);
 	}
 
 	render () {
-		return( <nav><a href="">{this.state.namee.map(this.shook)}</a></nav>
+		return( <nav>{this.state.namee.map(this.shook)}</nav>
 		);
 	}
 }
@@ -31,24 +33,44 @@ class Menu extends Component {
 
 class Panel extends Component {
 
-	openNav(e) {
+	constructor(props) {
+	  super(props);
+
+	  // You need to bind your functions in the constructor
+	  this.openNav = this.openNav.bind(this);
+	  this.closeNav = this.closeNav.bind(this);
+	  this.onClick= this.onClick.bind(this);
+		this.state = {open: false};
+	}
+
+
+	openNav() {
 	    document.getElementById("mySidenav").style.width = "250px";
 	    document.getElementById("container").style.marginLeft = "225px";
-	    document.getElementById( "panelinout" ).setAttribute( "onClick", {this.closeNav.bind(this)} );
-	    document.getElementById("panelinout").className = "hamburger change";
+	    //document.getElementById( "panelinout" ).setAttribute( "onClick", {closeNav} );
+			this.setState({open: true});
+			document.getElementById("panelinout").className = "hamburger change";
 	}
 
-	closeNav(e) {
+	closeNav() {
 	    document.getElementById("mySidenav").style.width = "50px";
 	    document.getElementById("container").style.marginLeft = "20px";
-	    document.getElementById("panelinout" ).setAttribute( "onClick", {this.openNav.bind(this)} );
-	    document.getElementById("panelinout").className = "hamburger";
+	    //document.getElementById("panelinout" ).setAttribute( "onClick", {openNav} );
+			this.setState({open: false});
+			document.getElementById("panelinout").className = "hamburger";
 	}
 
+	onClick() {
+	  if (this.state.open) {
+	    this.closeNav();
+	  } else {
+	    this.openNav();
+	  }
+	}
 
 	render () {
 		return (
-			<div id="panelinout" className="hamburger" onClick= {this.openNav.bind(this)} >
+			<div id="panelinout" className="hamburger" onClick= {this.onClick} >
 				<div className="bar1"></div>
 				<div className="bar2"></div>
 				<div className="bar3"></div>
